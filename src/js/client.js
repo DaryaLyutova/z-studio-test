12c5a74b8112a27c79eceb16c9e70b69810d7f3e
Darya Lutuva <lutowa.darya@yandex.ru>
2021-04-06 11:16:32 +0300

    fixed: section client

--- src/js/client.js
+++ src/js/client.js
@@ -1,13 +1,18 @@
-const client = document.querySelector('.client__list')
-const clientMobile = document.querySelector('.client__list-mobile')
+// const client = document.querySelector('.client__list')
+// const clientMobile = document.querySelector('.client__list-mobile')
+let screen = true;
 
 setTimeout(function random() {
     if (window.screen.availWidth < 768) {
-        clientMobile.classList.add('client__list-mobile_visible');
-        client.classList.remove('client__list_visible');
+        screen = false
+        // clientMobile.classList.add('client__list-mobile_visible');
+        // client.classList.remove('client__list_visible');
     } else {
-        client.classList.add('client__list_visible');
-        clientMobile.classList.remove('client__list-mobile_visible');
+        screen = true
+        // client.classList.add('client__list_visible');
+        // clientMobile.classList.remove('client__list-mobile_visible');
     }
     setTimeout(random, 1000);
 }, 1000);
+
+module.exports.screen = screen;
